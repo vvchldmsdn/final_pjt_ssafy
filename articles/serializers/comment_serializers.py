b53cf361eb3bcfb0fffc04e84d8a93dfb2d048ce
vvchldmsdn <vvchldmsdn@gmail.com>
2022-05-23 17:52:06 +0900

    0523 articles

--- articles/serializers/comment_serializers.py
+++ articles/serializers/comment_serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from ..models import Article, Comment
+from django.contrib.auth import get_user_model
+from ..models import Comment
 
+User = get_user_model()
 
+# CUD => validation
+# R => Data serializing
 class CommentSerializer(serializers.ModelSerializer):
+    
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ('pk', 'username')
+
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ('pk', 'user', 'content', 'article',)
+        read_only_fields = ('article', )
